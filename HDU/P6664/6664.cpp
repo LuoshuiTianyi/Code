bbfd61c9bd43a85b6b745b0d1bc6c2a16b36881f
LuoshuiTianyi <13574831631@163.com>
2019-09-12 15:20:02 +0800

    updated

---
+++ HDU/P6664/6664.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(x, i, v) for (int i = hd[x], v = to[i]; i; v = to[i = nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+LL read() {
+  long long x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) w = ch == '-' ? -1 : 1, ch = getchar();
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("6664.in", "r", stdin);
+  freopen("6664.out", "w", stdout);
+#endif
+}
