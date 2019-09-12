3d0a4e2cdb183b4815905868ed8253b33d1147df
LuoshuiTianyi <13574831631@163.com>
2019-09-12 15:40:01 +0800

    updated

--- HDU/P6664/6664.cpp
+++ HDU/P6664/6664.cpp
@@ -17,12 +17,23 @@ inline LL read() {
 const int mod = 1e9 + 7, Max_n = 5e4 + 5;
 int T;
 int n, m, Q;
+int pw[Max_n], f[Max_n][20];
 char S[Max_n];
 inline void Input() {
   n = read(), m = read(), Q = read();
   scanf("%s", S + 1);
 }
+inline void get_to(int i, int j, int k) {
+  return (k * pw[i] + j) % m;
+}
 inline void DP() {
+  pw[1] = 1, f[0][0] = 1;
+  for (int i = 2; i <= n; i++) pw[i] = pw[i - 1] * 10 % m;
+  for (int i = 1; i <= n; i++)
+    for (int j = 0; j < m; j++)
+      for (int k = 0; k <= 9; k++) {
+        int to = get_to(i, j, k);
+      }
 }
 int main() {
 #ifndef ONLINE_JUDGE
