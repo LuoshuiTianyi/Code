e9e8b9746518b0a52b1f6b22370b6c8d87333527
LuoshuiTianyi <13574831631@163.com>
2019-09-13 09:20:01 +0800

    updated

--- Luogu/P3232/3232.cpp
+++ Luogu/P3232/3232.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdio>
 #include <iostream>
 using namespace std;
@@ -17,8 +18,8 @@ inline LL read() {
 const int Max_n = 505;
 int n, m;
 int cntr, hd[Max_n], nx[Max_n * Max_n << 1], to[Max_n * Max_n << 1];
-int u[Max_n * Max_n], v[Max_n * Max_n];
-double in[Max_n], f[Max_n][Max_n], ans[Max_n];
+int u[Max_n * Max_n], v[Max_n * Max_n], num[Max_n * Max_n];
+double Ans, in[Max_n], f[Max_n][Max_n], ans[Max_n];
 inline void addr(int u, int v) {
   cntr++;
   nx[cntr] = hd[u], to[cntr] = v;
@@ -35,7 +36,7 @@ inline void Elimi() {
         for (int k = i; k <= n; k++) f[j][k] -= f[i][k] * x;
       }
   }
-  for (int i = 1; i < n; i++) ans[i] = f[i][n] / f[i][i];
+  for (int i = 1; i < n; i++) ans[i] = (f[i][n] / f[i][i]) / in[i];
 }
 int main() {
 #ifndef ONLINE_JUDGE
@@ -53,4 +54,9 @@ int main() {
   }
   f[1][n] = 1;
   Elimi();
+  for (int i = 1; i <= m; i++) num[i] = ans[u[i]] + ans[v[i]];
+  sort(num + 1, num + m + 1);
+  for (int i = 1; i <= m; i++)
+    Ans += num[i] * (m - i + 1);
+  printf("%.3lf", Ans);
 }
