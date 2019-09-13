57a9318695ba6ac6ad95e757dc832ef20219702e
LuoshuiTianyi <13574831631@163.com>
2019-09-13 08:30:02 +0800

    updated

---
+++ Luogu/P3389/3389.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(x, i, v) for (int i = hd[x], v = to[i]; i; v = to[i = nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+LL read() {
+  long long x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) w = ch == '-' ? -1 : 1, ch = getchar();
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+const int Max_n = 105;
+int n;
+double a[Max_n][Max_n];
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("3389.in", "r", stdin);
+  freopen("3389.out", "w", stdout);
+#endif
+  n = read();
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= n + 1; j++) a[i][j] = read();
+  for (int i = 1; i <= n; i++) {
+    int mp = i;
+    for (int j = i; j <= n; j++) abs(a[mp][i]) < abs(a[j][i]) ? mp = j : mp;
+    for (int j = i; j <= n + 1; j++) swap(a[i][j], a[mp][j]);
+    if (!a[i][i]) {
+      cout << "No Solution";
+      return 0;
+    }
+  }
+}
