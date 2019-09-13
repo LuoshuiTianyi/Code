ed75dee89715b49e49e2ea83911d7603bce62f8c
LuoshuiTianyi <13574831631@163.com>
2019-09-13 09:10:02 +0800

    updated

--- Luogu/P3232/3232.cpp
+++ Luogu/P3232/3232.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define LL long long
 #define go(x, i, v) for (int i = hd[x], v = to[i]; i; v = to[i = nx[i]])
 #define inline __inline__ __attribute__((always_inline))
-LL read() {
+inline LL read() {
   long long x = 0, w = 1;
   char ch = getchar();
   while (!isdigit(ch)) w = ch == '-' ? -1 : 1, ch = getchar();
@@ -17,11 +17,25 @@ LL read() {
 const int Max_n = 505;
 int n, m;
 int cntr, hd[Max_n], nx[Max_n * Max_n << 1], to[Max_n * Max_n << 1];
-double in[Max_n], f[Max_n][Max_n];
-void addr(int u, int v) {
+int u[Max_n * Max_n], v[Max_n * Max_n];
+double in[Max_n], f[Max_n][Max_n], ans[Max_n];
+inline void addr(int u, int v) {
   cntr++;
   nx[cntr] = hd[u], to[cntr] = v;
   hd[u] = cntr;
+}
+inline void Elimi() {
+  for (int i = 1, mp = 1; i < n; mp = ++i) {
+    for (int j = i + 1; j < n; j++)
+      if (f[j][i] > f[mp][i]) mp = j;
+    for (int j = i; j <= n; j++) swap(f[i][j], f[mp][j]);
+    for (int j = 1; j < n; j++)
+      if (i != j) {
+        double x = f[j][i] / f[i][i];
+        for (int k = i; k <= n; k++) f[j][k] -= f[i][k] * x;
+      }
+  }
+  for (int i = 1; i < n; i++) ans[i] = f[i][n] / f[i][i];
 }
 int main() {
 #ifndef ONLINE_JUDGE
@@ -29,14 +43,14 @@ int main() {
   freopen("3232.out", "w", stdout);
 #endif
   n = read(), m = read();
-  int u, v;
   for (int i = 1; i <= m; i++) {
-    in[u = read()]++, in[v = read()]++;
-    addr(u, v), addr(v, u);
+    in[u[i] = read()]++, in[v[i] = read()]++;
+    addr(u[i], v[i]), addr(v[i], u[i]);
   }
   for (int i = 1; i < n; i++) {
     f[i][i] = 1;
     go(i, j, v) if (v != n) f[i][v] = -1.0 / in[v];
   }
   f[1][n] = 1;
+  Elimi();
 }
