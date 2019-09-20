21a2645585c266519ca41298122b9b29cd27939c
LuoshuiTianyi <13574831631@163.com>
2019-09-20 15:30:01 +0800

    updated

--- Luogu/P2408/2408_SA.cpp
+++ Luogu/P2408/2408_SA.cpp
@@ -5,14 +5,10 @@ int n, M, cnt, rk[100001], tmp[100001], sa[100001], tag[100001], height[100001];
 long long ans;
 char s[100002];
 void Sort() {
-  for (int i = 1; i <= M; i++)
-    tag[i] = 0;
-  for (int i = 1; i <= n; i++)
-    tag[rk[i]]++;
-  for (int i = 1; i <= M; i++)
-    tag[i] += tag[i - 1];
-  for (int i = n; i >= 1; i--)
-    sa[tag[rk[tmp[i]]]--] = tmp[i];
+  for (int i = 1; i <= M; i++) tag[i] = 0;
+  for (int i = 1; i <= n; i++) tag[rk[i]]++;
+  for (int i = 1; i <= M; i++) tag[i] += tag[i - 1];
+  for (int i = n; i >= 1; i--) sa[tag[rk[tmp[i]]]--] = tmp[i];
 }
 int main() {
   freopen("2408.in", "r", stdin);
@@ -20,16 +16,13 @@ int main() {
   cin >> n;
   scanf("%s", s + 1);
   M = 76;
-  for (int i = 1; i <= n; i++)
-    rk[i] = s[i] - '0', tmp[i] = i;
+  for (int i = 1; i <= n; i++) rk[i] = s[i] - '0', tmp[i] = i;
   Sort();
   for (int len = 1; cnt < n; len <<= 1, M = cnt) {
     cnt = 0;
-    for (int i = n - len + 1; i <= n; i++)
-      tmp[++cnt] = i;
+    for (int i = n - len + 1; i <= n; i++) tmp[++cnt] = i;
     for (int i = 1; i <= n; i++)
-      if (sa[i] > len)
-        tmp[++cnt] = sa[i] - len;
+      if (sa[i] > len) tmp[++cnt] = sa[i] - len;
     Sort();
     swap(rk, tmp);
     rk[sa[1]] = cnt = 1;
@@ -40,14 +33,11 @@ int main() {
                       : ++cnt;
   }
   for (int i = 1, len = 0; i <= n; i++) {
-    if (len)
-      len--;
+    if (len) len--;
     int now = sa[rk[i] - 1];
-    while (s[now + len] == s[i + len])
-      len++;
+    while (s[now + len] == s[i + len]) len++;
     height[rk[i]] = len;
   }
-  for (int i = 1; i <= n; i++)
-    ans += n - sa[i] + 1 - height[i];
+  for (int i = 1; i <= n; i++) ans += n - sa[i] + 1 - height[i];
   cout << ans;
 }
