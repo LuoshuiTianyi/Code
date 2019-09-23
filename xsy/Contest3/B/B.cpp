ca5ba7aa385a2a4dc1069df5cb73163c7bb1487f
LuoshuiTianyi <13574831631@163.com>
2019-09-23 08:30:01 +0800

    updated

--- xsy/Contest3/B/B.cpp
+++ xsy/Contest3/B/B.cpp
@@ -17,13 +17,21 @@ inline LL read() {
 const int Max_n = 1e4 + 5;
 const LL lim = 1e17, inf = 1e18;
 int n;
-LL a[Max_n], b[Max_n];
+LL m, f[64][2][3], a[Max_n], b[Max_n];
+LL DP(int len, bool lim, int res) {
+  if (!len) return 1;
+}
+void work(LL L, LL R, LL a, LL b) {
+  m = b + lim;
+  memset(f, -1, sizeof(f));
+  DP(63, 1, 0);
+}
 bool check(LL x, LL y) {
   bool ans = 0;
   for (int i = 1; i <= n; i++) {
-    LL m = b[i] - y, n = x - a[i];
-    if (m < n || m < 0 || n < 0) continue;
-    ans ^= ((n & m) == n);
+    LL M = b[i] - y, N = x - a[i];
+    if (M < N || M < 0 || M < 0) continue;
+    ans ^= ((N & M) == N);
   }
   return ans;
 }
@@ -34,6 +42,5 @@ int main() {
 #endif
   n = read();
   for (int i = 1; i <= n; i++) a[i] = read(), b[i] = read();
-  
   LL l = -inf, r = inf, ans;
 }
