4c5932f58eeaa4a2d3344224d12d6bb5b4f4d08f
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-09-25 20:28:56 +0800

    changed

--- Luogu/P3391/3391.cpp
+++ Luogu/P3391/3391.cpp
@@ -1,83 +1,80 @@
 #include <cstdio>
 #include <iostream>
 using namespace std;
-#define ls s[0]
-#define rs s[1]
-int n, m, cnt, a[100003], sum;
-int rt, L, R;
-struct tree {
-  int s[2], fa, id, key, size;
-  bool tag;
-} k[100003];
-void update(int x) { k[x].size = k[k[x].ls].size + k[k[x].rs].size + 1; }
-void pushdown(int x) {
-  if (!k[x].tag) return;
-  k[k[x].ls].tag ^= 1, k[k[x].rs].tag ^= 1;
-  swap(k[x].ls, k[x].rs);
-  k[x].tag = false;
+#define LL long long
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
 }
+const int Max_n = 1e5 + 5;
+int n, m;
+int L, R, a[Max_n];
+namespace Splay {
+int cnt, rt;
+#define ls k[x].s[0]
+#define rs k[x].s[1]
+struct node {
+  int fa, val, s[2], size;
+  bool tag;
+} k[Max_n];
+void upd(int x) { k[x].size = k[ls].size + k[rs].size + 1; }
 int build(int l, int r, int fa) {
   if (l > r) return 0;
-  int mid = (l + r) >> 1, now = ++cnt;
-  k[now].id = now, k[now].fa = fa, k[now].key = a[mid], k[now].size = 1;
-  k[now].ls = build(l, mid - 1, now), k[now].rs = build(mid + 1, r, now);
-  if (now) update(now);
-  return now;
+  int x = ++cnt, mid = l + r >> 1;
+  k[x].fa = fa, k[x].val = a[mid], k[x].size = 1;
+  ls = build(l, mid - 1, x), rs = build(mid + 1, r, x), upd(x);
+  return x;
 }
-int find(int x) {
-  int now = rt;
-  while (1) {
-    pushdown(now);
-    if (x <= k[k[now].ls].size)
-      now = k[now].ls;
+int find(int res) {
+  int x = rt;
+  while (1)
+    if (k[ls].size < res)
+      x = ls;
     else {
-      x -= k[k[now].ls].size + 1;
-      if (!x) return now;
-      now = k[now].rs;
+      res -= k[ls].size;
+      if (!res) return x;
+      res--, x = rs;
     }
-  }
 }
-bool kind(int x) { return k[k[x].fa].rs == x; }
+int kd(int x) { return k[k[x].fa].rs == x; }
+void pushdown(int x) {
+  k[ls].tag ^= k[x].tag, k[rs].tag ^= k[x].tag;
+  if (k[x].tag) swap(ls, rs), k[x].tag = 0;
+}
 void rotate(int x) {
-  int f = k[x].fa, ff = k[f].fa;
-  pushdown(x), pushdown(f);
-  bool flag = kind(x);
-  k[f].s[flag] = k[x].s[flag ^ 1];
-  k[k[f].s[flag]].fa = f;
-  k[f].fa = x;
-  k[x].fa = ff;
-  k[x].s[flag ^ 1] = f;
-  if (ff) k[ff].s[k[ff].s[1] == f] = x;
-  update(f);
+  
 }
-void Splay(int x, int To) {
-  for (int Fa; (Fa = k[x].fa) != To; rotate(x)) {
-    if (k[Fa].fa != To) rotate(kind(x) ^ kind(Fa) ? x : Fa);
-  }
-  if (!To) rt = x;
+void splay(int x, int to) {
+  for (int i = fa; (fa = k[x].fa) != to; rotate(x))
+    if (k[fa].fa != to) rotate(kd(x) ^ kd(fa) ? x : fa);
+  if (!to) rt = x;
 }
-void revers(int l, int r) {
-  l = find(l), r = find(r + 2);
-  Splay(l, 0);
-  Splay(r, l);
-  k[k[k[rt].rs].ls].tag ^= 1;
+void Reverse() { 
+  L = find(L), R = find(R + 2); 
+  splay(L, 0), splay(R, L);
 }
-void print(int x) {
-  pushdown(x);
-  if (k[x].ls) print(k[x].ls);
-  if (k[x].key != -1e9 && k[x].key != 1e9) printf("%d ", k[x].key);
-  if (k[x].rs) print(k[x].rs);
-}
+}  // namespace Splay
 int main() {
+#ifndef ONLINE_JUDGE
   freopen("3391.in", "r", stdin);
   freopen("3391.out", "w", stdout);
-  cin >> n >> m;
+#endif
+  n = read(), m = read();
   a[1] = -1e9, a[n + 2] = 1e9;
-  for (int i = 1; i <= n; i++) a[i + 1] = i;
-  rt = build(1, n + 2, 0);
+  for (int i = 2; i <= n + 1; i++) a[i] = i - 1;
+  Splay::rt = Splay::build(1, n, 0);
   while (m--) {
-    scanf("%d%d", &L, &R);
-    revers(L, R);
+    L = read(), R = read();
+    Splay::Reverse();
   }
-  print(rt);
 }
