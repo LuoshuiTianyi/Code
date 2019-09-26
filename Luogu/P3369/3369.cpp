6966ae3e81007f321d2ed9bb6ce6fbf39e81fa16
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-09-26 09:00:03 +0800

    changed

--- Luogu/P3369/3369.cpp
+++ Luogu/P3369/3369.cpp
@@ -27,27 +27,46 @@ struct node {
   int fa, v, size, s[2];
 } k[Max_n];
 bool kd(int x) { return rs(k[x].fa) == x; }
-void pushdown(int x) { k[x].size = k[ls(x)].size + k[rs(x)].size + 1; }
+void upd(int x) { k[x].size = k[ls(x)].size + k[rs(x)].size + 1; }
 int nx(int x) { return n <= k[x].v ? ls(x) : rs(x); }
-void add() {
-  int x = rt;
-  if (!x) rt = cnt + 1;
-  while (nx(x)) x = nx(x);
-  cnt++, k[cnt].fa = x, k[cnt].v = n;
-  if (n <= k[x].v)
-    ls(x) = cnt;
-  else
-    rs(x) = cnt;
+inline void rotate(int x) {
+  bool t = kd(x);
+  int A = k[x].fa, B = k[A].fa, C = k[x].s[!t];
+  k[A].s[t] = C, k[B].s[kd(A)] = x, k[x].s[!t] = A;
+  k[A].fa = x, k[x].fa = B, k[C].fa = A;
+  upd(A), upd(x);
 }
-void rotate(int x) {
-  
-}
-void splay(int x, int to) {
+inline void splay(int x, int to) {
   for (int fa; (fa = k[x].fa) != to; rotate(x))
     if (k[fa].fa != to) rotate(kd(x) ^ kd(fa) ? x : fa);
   if (!to) rt = x;
 }
-void del() {
+inline void add() {
+  int x = rt;
+  if (!x) rt = cnt + 1;
+  while (nx(x)) x = nx(x);
+  cnt++, k[cnt].fa = x, k[cnt].v = n, k[cnt].size = 1;
+  if (n <= k[x].v)
+    ls(x) = cnt;
+  else
+    rs(x) = cnt;
+  upd(x), splay(cnt, rt);
+}
+inline int find(int x) {
+  int p;
+  for (p = rt; k[p].v != x; p = k[p].v > x ? ls(p) : rs(p))
+    ;
+  return p;
+}
+inline void del(int x) {
+  splay(x = find(x), 0);
+  if (!ls(x)) rt = rs(x);
+  else {
+    int q = ls(x);
+    while (rs(q)) q = rs(q);
+    splay(q, x);
+    rt = q, rs(q) = rs(x), k[rs(x)].fa = q, upd(q);
+  }
 }
 }  // namespace Splay
 int main() {
