e82c6b04c24a5caa1971e3a0242d7c71b4087c84
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-09-26 09:30:18 +0800

    changed

--- Luogu/P3369/3369.cpp
+++ Luogu/P3369/3369.cpp
@@ -19,16 +19,19 @@ inline LL read() {
 const int Max_n = 1e5 + 5;
 int m;
 int opt, n;
+int p;
 namespace Splay {
 int cnt, rt;
 #define ls(x) k[x].s[0]
 #define rs(x) k[x].s[1]
 struct node {
-  int fa, v, size, s[2];
+  int fa, v, size, cnt, s[2];
 } k[Max_n];
 bool kd(int x) { return rs(k[x].fa) == x; }
-void upd(int x) { k[x].size = k[ls(x)].size + k[rs(x)].size + 1; }
-int nx(int x) { return n <= k[x].v ? ls(x) : rs(x); }
+void upd(int x) {
+  if (!x) return;
+  k[x].size = k[ls(x)].size + k[rs(x)].size + k[x].cnt;
+}
 inline void rotate(int x) {
   bool t = kd(x);
   int A = k[x].fa, B = k[A].fa, C = k[x].s[!t];
@@ -43,30 +46,58 @@ inline void splay(int x, int to) {
 }
 inline void add() {
   int x = rt;
-  if (!x) rt = cnt + 1;
-  while (nx(x)) x = nx(x);
+  if (!x) x = rt = cnt + 1;
+  for (; k[x].v != n && x;) x = k[x].v > n ? ls(x) : rs(x);
+  if (x) {
+    k[x].cnt++, k[x].size++;
+    return;
+  }
   cnt++, k[cnt].fa = x, k[cnt].v = n, k[cnt].size = 1;
-  if (n <= k[x].v)
+  if (n < k[x].v)
     ls(x) = cnt;
   else
     rs(x) = cnt;
-  upd(x), splay(cnt, rt);
+  upd(cnt), splay(cnt, rt);
 }
-inline int find(int x) {
-  int p;
-  for (p = rt; k[p].v != x; p = k[p].v > x ? ls(p) : rs(p))
-    ;
-  return p;
-}
-inline void del(int x) {
-  splay(x = find(x), 0);
-  if (!ls(x)) rt = rs(x);
+inline void del() {
+  int x = rt;
+  for (; k[x].v != n;) x = k[x].v > n ? ls(x) : rs(x);
+  if (--k[x].cnt) return;
+  splay(x, 0);
+  if (!ls(x))
+    rt = rs(x);
   else {
     int q = ls(x);
     while (rs(q)) q = rs(q);
     splay(q, x);
     rt = q, rs(q) = rs(x), k[rs(x)].fa = q, upd(q);
   }
+}
+inline int rak() {
+  int res = 0, x = rt;
+  for (; k[x].v != n; x = k[x].v > n ? ls(x) : rs(x))
+    if (k[x].v < n) res += k[ls(x)].size + 1;
+  return res + k[ls(x)].size;
+}
+inline int num() {
+  int x = rt;
+  while (1)
+    if (k[ls(x)].size < n) {
+      n -= k[ls(x)].size;
+      if (!(--n)) return k[x].v;
+      x = rs(x);
+    } else
+      x = ls(x);
+}
+inline int pre() {
+  int x = rt;
+  for (; k[x].v != n;) x = k[x].v > n ? ls(x) : rs(x);
+  splay(x, 0);
+}
+inline int nxt() {
+  int x = rt;
+  for (; k[x].v != n;) x = k[x].v > n ? ls(x) : rs(x);
+  splay(x, 0);
 }
 }  // namespace Splay
 int main() {
@@ -77,7 +108,11 @@ int main() {
   m = read();
   while (m--) {
     opt = read(), n = read();
-    if (opt == 1) {
-    }
+    if (opt == 1) Splay::add();
+    if (opt == 2) Splay::del();
+    if (opt == 3) printf("%d\n", Splay::rak());
+    if (opt == 4) printf("%d\n", Splay::num());
+    if (opt == 5) printf("%d\n", Splay::pre());
+    if (opt == 6) printf("%d\n", Splay::nxt());
   }
 }
