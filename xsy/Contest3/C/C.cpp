b413ce72871fd40132fcb8868a5740f5e94cdaa2
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-09-29 19:30:32 +0800

    changed

---
+++ xsy/Contest3/C/C.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+#define LL long long
+#define go(x, i, v) for (int i = hd[x], v = to[i]; i; v = to[i = nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+const int Max_n = 1e5 + 5;
+struct node {
+  int fa, 
+}
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("C.in", "r", stdin);
+  freopen("C.out", "w", stdout);
+#endif
+}
