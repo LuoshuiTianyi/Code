934ddd44ba0db0437549e5d6d0d705822ed652a5
luoshuitianyi <CSYcaosiyu@gmail.com>
2019-10-12 15:41:01 +0800

    changed

---
+++ Luogu/P4897/4897.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(x, i, v) for (int i = hd[x], v = to[i]; i; v = to[i = nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+int n, m;
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("4897.in", "r", stdin);
+  freopen("4897.out", "w", stdout);
+#endif
+}
