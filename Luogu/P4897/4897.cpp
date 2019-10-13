192726ca6029f211d02c38e0c5a463fc7832c769
luoshuitianyi <CSYcaosiyu@gmail.com>
2019-10-13 19:56:01 +0800

    changed

--- Luogu/P4897/4897.cpp
+++ Luogu/P4897/4897.cpp
@@ -113,6 +113,7 @@ int main() {
   for (int i = 0; i <= n; i++) node[i] = i;
   Solve(0, n);
   build(0, -1);
+  cout << Min[3][0] << endl;
   for (int j = 1; j < 10; j++)
     for (int i = 0; i <= n; i++) {
       f[j][i] = f[j - 1][f[j - 1][i]];
