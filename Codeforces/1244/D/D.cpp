319bc9fece246e8aca79f7c629212df29004d1f0
luoshuitianyi <CSYcaosiyu@gmail.com>
2019-10-13 17:30:01 +0800

    changed

---
+++ Codeforces/1244/D/D.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+const int Max_n = 1e5 + 5;
+int n;
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("D.in", "r", stdin);
+  freopen("D.out", "w", stdout);
+#endif
+}
