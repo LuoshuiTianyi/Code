08581c77f3addf6e98aa978fd772454a74f9974c
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-10-31 20:10:01 +0800

    changed

--- Luogu/P1850/1850.cpp
+++ Luogu/P1850/1850.cpp
@@ -1,54 +1,49 @@
 #include <cstdio>
-#include <cstring>
 #include <iostream>
 using namespace std;
-int n, m, v, e, c[2001], d[2001];
-double odd[2001], f[2001][2001][2], dis[301][301], inf = 1e18;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+
+const int Max_n = 2005;
+int n, m, v, e;
+int a[Max_n], b[Max_n];
+double k[Max_n];
+int dis[Max_n][Max_n];
+
+namespace Input {
+void main() {
+  n = read(), m = read(), v = read(), e = read();
+  for (int i = 1; i <= n; i++) a[i] = read();
+}
+}  // namespace Input
+
+namespace Init {
+void main() {}
+}  // namespace Init
+
+namespace Solve {
+void main() {}
+}  // namespace Solve
+
 int main() {
+#ifndef ONLINE_JUDGE
   freopen("1850.in", "r", stdin);
   freopen("1850.out", "w", stdout);
-  cin >> n >> m >> v >> e;
-  for (int i = 1; i <= n; i++)
-    cin >> c[i];
-  for (int i = 1; i <= n; i++)
-    cin >> d[i];
-  for (int i = 1; i <= n; i++)
-    cin >> odd[i];
-  for (int i = 0; i <= n; i++)
-    for (int j = 0; j <= m; j++)
-      f[i][j][1] = f[i][j][0] = inf;
-  for (int i = 1; i <= v; i++)
-    for (int j = 1; j <= v; j++)
-      if (i != j)
-        dis[i][j] = inf;
-  for (int i = 1; i <= e; i++) {
-    int u, v;
-    double w;
-    cin >> u >> v >> w;
-    dis[u][v] = dis[v][u] = min(dis[u][v], w);
-  }
-  for (int k = 1; k <= v; k++)
-    for (int i = 1; i <= v; i++)
-      for (int j = 1; j <= v; j++)
-        dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
-  f[1][0][0] = f[1][1][1] = 0;
-  for (int i = 2; i <= n; i++) {
-    for (int j = 0; j <= min(i, m); j++) {
-      if (j)
-        f[i][j][1] =
-            min(f[i - 1][j - 1][0] + odd[i] * dis[c[i - 1]][d[i]] +
-                    (1 - odd[i]) * dis[c[i - 1]][c[i]],
-                f[i - 1][j - 1][1] + odd[i] * odd[i - 1] * dis[d[i - 1]][d[i]] +
-                    (1 - odd[i]) * odd[i - 1] * dis[d[i - 1]][c[i]] +
-                    odd[i] * (1 - odd[i - 1]) * dis[c[i - 1]][d[i]] +
-                    (1 - odd[i]) * (1 - odd[i - 1]) * dis[c[i - 1]][c[i]]);
-      f[i][j][0] = min(f[i - 1][j][0] + dis[c[i - 1]][c[i]],
-                       f[i - 1][j][1] + odd[i - 1] * dis[d[i - 1]][c[i]] +
-                           (1 - odd[i - 1]) * dis[c[i - 1]][c[i]]);
-    }
-  }
-  double ans = inf;
-  for (int i = 0; i <= m; i++)
-    ans = min(ans, min(f[n][i][1], f[n][i][0]));
-  printf("%.2lf", ans);
+#endif
+  Input::main(), Init::main(), Solve::main();
 }
