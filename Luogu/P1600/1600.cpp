f79f1dd8db12564e641aaeeba413292d1b460fb2
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-11-01 09:50:01 +0800

    changed

--- Luogu/P1600/1600.cpp
+++ Luogu/P1600/1600.cpp
@@ -22,9 +22,9 @@ inline LL read() {
 
 const int Max_n = 3e5 + 5, P = 3e5 + 5;
 int n, m, Ans[Max_n];
-int s[Max_n], t[Max_n], lca[Max_n], w[Max_n], fa[Max_n], buk[Max_n << 1];
+int s[Max_n], t[Max_n], lca[Max_n], w[Max_n], fa[Max_n], buk[2][Max_n << 1];
 int dep[Max_n];
-vector<int> M[Max_n], ad1[Max_n], de1[Max_n], ad2[Max_n], de2[Max_n];
+vector<int> M[Max_n], add[2][Max_n], del[2][Max_n];
 struct graph {
   int hd[Max_n];
   int cntr, nx[Max_n << 1], to[Max_n << 1];
@@ -52,7 +52,7 @@ namespace Init {
 int find(int x) { return fa[x] == x ? x : fa[x] = find(fa[x]); }
 void build(int x, int f) {
   dep[x] = dep[f] + 1;
-  go(G, x, i, v) if (v != f) build(v, f);
+  go(G, x, i, v) if (v != f) build(v, x);
   for (int i = M[x].size() - 1; ~i; i--) {
     int j = M[x][i];
     if (find(s[j]) != s[j] || find(t[j]) != t[j]) 
@@ -64,8 +64,8 @@ void main() {
   for (int i = 1; i <= m; i++) {
     int u = s[i], v = t[i], l = lca[i];
     int d1 = dep[u], d2 = dep[u] - (dep[l] << 1) + P;
-    ad1[l].push_back(d1), de1[u].push_back(d1);
-    de2[l].push_back(d2), ad2[v].push_back(d2);
+    add[0][l].push_back(d1), del[0][u].push_back(d1);
+    del[1][l].push_back(d2), add[1][v].push_back(d2);
     Ans[l] -= (dep[u] - dep[l] == w[l]);
   }
 }
@@ -73,11 +73,18 @@ void main() {
 
 namespace Solve {
 void dfs(int x, int fa) {
-  int tp1 = 
-  go(G, x, i, v) if (v != fa) dfs(v, x);
+  //int s0 = w[x] + dep[x], s1 = w[x] - dep[x] + P;
+  //int t0 = buk[0][s0], t1 = buk[1][s1];
+  //for (int l = 0; l < 2; l++)
+  //  for (int i = add[l][x].size(); ~i; i--) buk[l][add[l][x][i]]++;
+  //go(G, x, i, v) if (v != fa) dfs(v, x);
+  //Ans[x] += buk[0][s0] + buk[1][s1] - t0 - t1;
+  //for (int l = 0; l < 2; l++)
+  //  for (int i = del[l][x].size(); ~i; i--) buk[l][del[l][x][i]]--;
 }
 void main() {
   dfs(1, 0);
+  for (int i = 1; i <= n; i++) printf("%d ", Ans[i]);
 }
 }  // namespace Solve
 
