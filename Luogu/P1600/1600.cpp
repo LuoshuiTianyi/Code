b8418b6fc904193eba8586af096313319d8c7102
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-11-01 09:10:01 +0800

    changed

--- Luogu/P1600/1600.cpp
+++ Luogu/P1600/1600.cpp
@@ -20,17 +20,50 @@ inline LL read() {
   return x * w;
 }
 
-const int Max_n = 3e5 + 5;
-int n, m;
-
-vector<int> Lca[Max_n], a1[Max_n], b1[Max_n], a2[Max_n], b2[Max_n];
+const int Max_n = 3e5 + 5, add = 3e5 + 5;
+int n, m, Ans[Max_n];
+int s[Max_n], t[Max_n], lca[Max_n], w[Max_n], fa[Max_n], buk[Max_n << 1];
+int dep[Max_n];
+vector<int> M[Max_n], a1[Max_n], b1[Max_n], a2[Max_n], b2[Max_n];
+struct graph {
+  int hd[Max_n];
+  int cntr, nx[Max_n << 1], to[Max_n << 1];
+  void addr(int u, int v) {
+    cntr++;
+    nx[cntr] = hd[u], to[cntr] = v;
+    hd[u] = cntr;
+  }
+} G;
 
 namespace Input {
-void main() {}
+void main() {
+  n = read(), m = read();
+  for (int i = 1; i < n; i++) {
+    int u = read(), v = read();
+    G.addr(u, v), G.addr(v, u);
+  }
+  for (int i = 1; i <= n; i++) w[i] = read(), fa[i] = i;
+  for (int i = 1; i <= m; i++) 
+    M[s[i] = read()].push_back(i), M[t[i] = read()].push_back(i);
+}
 }  // namespace Input
 
 namespace Init {
-void main() {}
+int find(int x) { return fa[x] == x ? x : fa[x] = find(fa[x]); }
+void build(int x, int f) {
+  dep[x] = dep[f] + 1;
+  go(G, x, i, v) if (v != f) build(v, f);
+  for (int i = M[x].size() - 1; ~i; i--) {
+    int j = M[x][i];
+    if (find(s[j]) != s[j] || find(t[j]) != t[j]) 
+      lca[j] = find(s[j]) != s[j] ? find(s[j]) : find(t[j]);
+  }
+}
+void main() {
+  build(1, 0);
+  for (int i = 1; i <= m; i++) {
+  }
+}
 }  // namespace Init
 
 namespace Solve {
