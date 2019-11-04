2429dd5fb28cc4c9f2cc0b996f79bede23b2c93d
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-11-04 22:20:09 +0800

    changed

--- Codeforces/449/D/D.cpp
+++ Codeforces/449/D/D.cpp
@@ -23,8 +23,6 @@ inline LL read() {
 const int Max_n = 1e6 + 5, Max = 1 << 20, mod = 1e9 + 7;
 int n, ans;
 int a[Max_n], f[Max + 1], g[100];
-int top, stk[20], nx[Max_n][20];
-void Mod(int &x) { x = x >= mod ? x - mod : x < 0 ? x + mod : x; }
 
 namespace Input {
 void main() {
@@ -34,29 +32,7 @@ void main() {
 }  // namespace Input
 
 namespace Init {
-int pos_now;
-bool cmp(int a, int b) { return nx[pos_now][a] < nx[pos_now][b]; }
 void main() {
-  for (int j = 0; j < 20; j++)
-    for (int i = n + 1; i; i--) nx[i][j] = (a[i] >> j & 1) ? nx[i + 1][j] : i;
-  for (int i = 1; i <= n; i++) {
-    top = 0, pos_now = i;
-    for (int j = 0; j < 20; j++)
-      if (a[i] >> j & 1) {
-        stk[++top] = j;
-        cout << i << " " << a[i] << " " << j << " " << nx[i][j] << endl;
-      }
-    stable_sort(stk + 1, stk + top + 1, cmp);
-    int now = a[i];
-    f[now] += nx[i][stk[1]] - i, now ^= (1 << stk[1]);
-    for (int j = 2; j <= top; j++) {
-      if (nx[i][stk[j]] != nx[i][stk[j - 1]]) {
-        f[now] += nx[i][stk[j]] - nx[i][stk[j - 1]];
-        //cout << i << " " << now << " " << stk[j - 1] << " " << nx[stk[j]] << endl;
-      }
-      now ^= (1 << stk[j]);
-    }
-  }
   for (int j = 0; j < 20; j++)
     for (int i = 0; i < Max; i++)
       if (!(i >> j & 1)) Mod(f[i] += f[i | (1 << j)]);
