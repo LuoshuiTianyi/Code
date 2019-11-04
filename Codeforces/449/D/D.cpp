7d370c95a7d6d4de6311e1696ee45d6cae09d684
luoshuitianyi <CSYcaosiyu@gmail.com>
2019-11-04 23:18:25 +0800

    Changed

--- Codeforces/449/D/D.cpp
+++ Codeforces/449/D/D.cpp
@@ -22,12 +22,13 @@ inline LL read() {
 
 const int Max_n = 1e6 + 5, Max = 1 << 20, mod = 1e9 + 7;
 int n, ans;
-int a[Max_n], f[Max + 1], g[100];
+int f[Max + 1], g[100];
+void Mod(int &x) { x = x < 0 ? x + mod : x >= mod ? x - mod : x; }
 
 namespace Input {
 void main() {
   n = read();
-  for (int i = 1; i <= n; i++) a[i] = read();
+  for (int i = 1; i <= n; i++) f[read()]++;
 }
 }  // namespace Input
 
@@ -40,8 +41,14 @@ void main() {
 }  // namespace Init
 
 namespace Solve {
+int ksm(int a, int b) {
+  int res = 1;
+  for (; b; b >>= 1, a = 1ll * a * a % mod)
+    if (b & 1) res = 1ll * res * a % mod;
+  return res;
+}
 void main() {
-  for (int i = 0; i < Max; i++) Mod(g[__builtin_popcount(i)] += f[i]);
+  for (int i = 0; i < Max; i++) Mod(g[__builtin_popcount(i)] += ksm(2, f[i]) - 1);
   for (int i = 0; i <= 20; i++)
     if (i & 1)
       Mod(ans -= g[i]);
