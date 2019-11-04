5b194734f82fe3deef3614b29a84108cc910fad7
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-11-04 21:30:01 +0800

    changed

---
+++ Codeforces/449/D/D.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+
+const int Max_n = 1e6 + 5, Max = 1 << 20, mod = 1e9 + 7;
+int n;
+int a[Max_n], f[Max + 1];
+
+namespace Input {
+void main() {
+  n = read();
+  for (int i = 1; i <= n; i++) f[read()]++;
+}
+}  // namespace Input
+
+namespace Init {
+void main() {}
+}  // namespace Init
+
+namespace Solve {
+void main() {}
+}  // namespace Solve
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("D.in", "r", stdin);
+  freopen("D.out", "w", stdout);
+#endif
+  Input::main();
+  Init::main();
+  Solve::main();
+}
