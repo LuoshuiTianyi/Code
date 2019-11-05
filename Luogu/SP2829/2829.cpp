c6e4814dde1e9cc7d967d59b0e2754c1bedb715e
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-11-05 08:30:01 +0800

    changed

--- Luogu/SP2829/2829.cpp
+++ Luogu/SP2829/2829.cpp
@@ -21,29 +21,39 @@ inline LL read() {
 
 const int Max = 1 << 15, mod = 1e9;
 int T;
-int n, all;
+int n, all, ans;
 int c[100];
 int f[Max + 1], g[Max + 1];
+void Mod(int &x) { x = x >= mod ? x - mod : x < 0 ? x + mod : x; }
 
 namespace Input {
-void main() { 
-  n = read(), all = (1 << read()) - 1; 
+void main() {
+  n = read(), all = (1 << read()) - 1;
   for (int i = 1; i <= n; i++) c[i] = read();
 }
 }  // namespace Input
 
+namespace Init {
+void main() {
+  f[0] = 1;
+  for (int i = 1; i <= all; i++) f[i] = 0;
+}
+}  // namespace Init
+
 namespace Solve {
 void init() {
-  swap(g, i);
-  for (int i = 0; i <= all; i++) f[i] = 0;
+  swap(g, f);
   for (int j = 0; j < 15; j++)
-    for (int i = all; ~i; i--)
-      if (i >> j & 1) 
+    for (int i = 0; i <= all; i++)
+      if (i >> j & 1) Mod(g[i] += g[i ^ (1 << j)]);
 }
 void main() {
   for (int i = 1; i <= n; i++) {
     init();
+    for (int j = 0; j <= all; j++) f[j] = j % c[i] != 0 ? g[j ^ all] : 0;
   }
+  for (int i = 0; i <= all; i++) Mod(ans += f[i]);
+  printf("%d\n", ans);
 }
 }  // namespace Solve
 
@@ -55,6 +65,7 @@ int main() {
   T = read();
   while (T--) {
     Input::main();
+    Init::main();
     Solve::main();
   }
 }
