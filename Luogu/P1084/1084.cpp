de378ec0d869bb3a6e6c218714cfbfd65ca8c1a3
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-11-13 20:40:08 +0800

    changed

--- Luogu/P1084/1084.cpp
+++ Luogu/P1084/1084.cpp
@@ -22,7 +22,8 @@ inline LL read() {
 
 const int Max_n = 5e4 + 5;
 int n, m;
-int f[Max_n], a[Max_n];
+LL ans;
+int t, f[Max_n], a[Max_n], s[Max_n];
 LL dep[Max_n];
 
 struct graph {
@@ -53,6 +54,7 @@ void build(int x, int fa) {
   go(G, x, i, v) if (v != fa) dep[v] = dep[x] + G.w[i], build(v, x);
 }
 bool cmp(int x, int y) { return dep[a[x]] < dep[a[y]]; }
+bool cmp2(int x, int y) { return dep[x] > dep[y]; }
 void DP(int x, int fa) {
   go(G, x, i, v) if (v != fa) {
     DP(v, fa);
@@ -61,13 +63,20 @@ void DP(int x, int fa) {
 }
 void main() {
   build(1, 0);
-  sort(a + 1, a + m + 1, cmp);
-  dep[0] = 1e18, DP(1, 0);
+  sort(a + 1, a + m + 1, cmp), dep[0] = 1e18;
+  go(G, 1, i, v) s[++t] = v;
+  sort(s + 1, s + t + 1, cmp2);
 }
 }  // namespace Init
 
 namespace Solve {
-void main() {}
+void main() {
+  if (m < t) {
+    puts("-1");
+    return;
+  }
+  LL l = 0, r = 1e18;
+}
 }  // namespace Solve
 
 int main() {
