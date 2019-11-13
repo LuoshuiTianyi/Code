baaddecf80e9dff44e85c297fe150352dee5e35b
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-11-13 11:40:01 +0800

    changed

--- Luogu/P2680/2680.cpp
+++ Luogu/P2680/2680.cpp
@@ -20,9 +20,8 @@ inline LL read() {
 }
 
 const int Max_n = 3e5 + 5;
-int n, m;
-int f[Max_n], c[Max_n];
-int s[Max_n], t[Max_n];
+int n, m, ans;
+int f[Max_n], dep[Max_n], c[Max_n];
 
 struct graph {
   int hd[Max_n];
@@ -33,6 +32,9 @@ struct graph {
     hd[u] = cntr;
   }
 } G, M;
+struct road {
+  int s, t, lca, dist;
+} k[Max_n];
 
 namespace Input {
 void main() {
@@ -42,18 +44,38 @@ void main() {
     u = read(), v = read(), w = read();
     G.addr(u, v, w), G.addr(v, u, w);
   }
-  for (int i = 1; i <= m; i++) s[i] = read(), t[i] = read();
+  for (int i = 1; i <= m; i++) k[i].lca = k[i].s = read(), k[i].t = read();
 }
 }  // namespace Input
 
 namespace Init {
+int find(int x) { return f[x] == x ? x : f[x] = find(f[x]); }
+void Get(int x, int fa) {
+  go(G, x, i, v) if (v != fa) {
+    dep[v] = dep[x] + G.w[i], Get(v, x), f[find(v)] = find(x);
+  }
+  go(M, x, i, v) if (find(k[v].s) != k[v].s) k[v].lca = find(k[v].s);
+  else if (find(k[v].t) != k[v].t) k[v].lca = find(k[v].t);
+}
+bool cmp(road a, road b) { return a.dist < b.dist; }
 void main() {
   for (int i = 1; i <= m; i++) M.addr(s[i], i), M.addr(t[i], i);
+  for (int i = 1; i <= n; i++) f[i] = i;
+  Get(1, 0);
+  for (int i = 1; i <= m; i++)
+    k[i].dist = dep[k[i].s] + dep[k[i].t] - (dep[k[i].lca] << 1);
+  sort(k + 1, k + m + 1, cmp);
 }
 }  // namespace Init
 
 namespace Solve {
-void main() {}
+void main() {
+  int l = 0, r = 3e8;
+  while (l <= r) {
+    int mid = l + r >> 1;
+    if (check(mid)) ans = mid, r = mid - 1;
+  }
+}
 }  // namespace Solve
 
 int main() {
