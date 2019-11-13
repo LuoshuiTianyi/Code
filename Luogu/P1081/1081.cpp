a9e6beb5ca6a762d7d0bcd893afce2b23ecefa21
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-11-13 09:30:02 +0800

    changed

--- Luogu/P1081/1081.cpp
+++ Luogu/P1081/1081.cpp
@@ -26,14 +26,19 @@ int n, Q, X, S;
 int h[Max_n], nu[Max_n];
 int bef[Max_n], nxt[Max_n], bel[Max_n];
 int to1[Max_n], to2[Max_n];
+
 struct sth {
   int Min, sMin;
 };
+struct jump {
+  int to[18], v[18];
+} A[Max_n], B[Max_n];
+
 void Max(sth &a, int b, int c) {
   int v = abs(h[b] - h[c]);
-  if (v <= abs(h[a.Min] - h[c]))
+  if (v < abs(h[a.Min] - h[c]))
     a.sMin = a.Min, a.Min = b;
-  else if (v <= abs(h[a.sMin] - h[c]))
+  else if (v < abs(h[a.sMin] - h[c]))
     a.sMin = b;
 }
 
@@ -50,13 +55,22 @@ bool cmp(int a, int b) { return h[a] < h[b]; }
 void main() {
   sort(nu + 1, nu + n + 1, cmp);
   for (int i = 1; i <= n; i++) bef[i] = i - 1, nxt[i] = i + 1, bel[nu[i]] = i;
+  h[0] = -1e9;
   for (int i = 1; i <= n; i++) {
+    sth S = (sth){0, 0};
+    int pos = bel[i];
+    Max(S, bef[pos], i), Max(S, bef[bef[pos]], i);
+    Max(S, nxt[pos], i), Max(S, nxt[nxt[pos]], i);
+    to1[i] = S.Min, to2[i] = S.sMin;
+    cout << to1[i] << " " << to2[i] << endl;
+    bef[nxt[pos]] = bef[pos], nxt[bef[pos]] = nxt[pos];
   }
 }
 }  // namespace Init
 
 namespace Solve {
-void main() {}
+void main() {
+}
 }  // namespace Solve
 
 int main() {
