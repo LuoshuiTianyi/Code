58b57cbba2b3cab82cc540e34cc0e15ed5df12e9
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-11-13 11:30:08 +0800

    changed

--- Luogu/P2680/2680.cpp
+++ Luogu/P2680/2680.cpp
@@ -21,13 +21,35 @@ inline LL read() {
 
 const int Max_n = 3e5 + 5;
 int n, m;
+int f[Max_n], c[Max_n];
+int s[Max_n], t[Max_n];
+
+struct graph {
+  int hd[Max_n];
+  int cntr, nx[Max_n << 1], to[Max_n << 1], w[Max_n << 1];
+  void addr(int u, int v, int W = 0) {
+    cntr++;
+    nx[cntr] = hd[u], to[cntr] = v, w[cntr] = W;
+    hd[u] = cntr;
+  }
+} G, M;
 
 namespace Input {
-void main() {}
+void main() {
+  n = read(), m = read();
+  int u, v, w;
+  for (int i = 1; i < n; i++) {
+    u = read(), v = read(), w = read();
+    G.addr(u, v, w), G.addr(v, u, w);
+  }
+  for (int i = 1; i <= m; i++) s[i] = read(), t[i] = read();
+}
 }  // namespace Input
 
 namespace Init {
-void main() {}
+void main() {
+  for (int i = 1; i <= m; i++) M.addr(s[i], i), M.addr(t[i], i);
+}
 }  // namespace Init
 
 namespace Solve {
