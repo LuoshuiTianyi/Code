118e802ae3c479a92da5772417b7f2b6d5c0e839
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-11-26 20:30:01 +0800

    changed

---
+++ Luogu/P5664/5664.cpp
@@ -0,0 +1,69 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 1) + (x << 3) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+
+const int Max_n = 2005, mod = 998244353;
+int n, m, ans;
+int a[200][Max_n], tot[200];
+
+namespace Input {
+void main() {
+  n = read(), m = read();
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= m; j++) (tot[i] += (a[i][j] = read())) %= mod;
+}
+}  // namespace Input
+
+namespace Init {
+void main() {
+  ans = 1;
+  for (int i = 1; i <= n; i++) ans = 1ll * ans * (tot[i] + 1) % mod;
+  ans = (ans - 1 + mod) % mod;
+}
+}  // namespace Init
+
+namespace Solve {
+int f[400], g[400];
+void main() {
+  for (int x = 1; x <= m; x++) {
+    g[n] = 1;
+    for (int i = 1; i <= n; i++) {
+      swap(f, g);
+      for (int i = 0; i <= 2 * n + 1; i++) g[i] = 0;
+      for (int j = 1; j <= 2 * n + 1; j++)
+        g[j - 1] = 1ll * f[i] * (tot[i] - a[i][x] + mod) % mod;
+      for (int j = 0; j < 2 * n + 1; j++)
+        (g[j + 1] += 1ll * f[i] * a[i][x] % mod) %= mod;
+    }
+    for (int i = n; i <= 2 * n + 1; i++) ans = (ans - g[i] + mod) % mod;
+  }
+  cout << ans;
+}
+}  // namespace Solve
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("5664.in", "r", stdin);
+  freopen("5664.out", "w", stdout);
+#endif
+  Input::main();
+  Init::main();
+  Solve::main();
+  return 0;
+}
