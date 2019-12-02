169e535e73f8db0ab54e03f5bd0a529f69be4f01
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-12-02 09:20:15 +0800

    changed

---
+++ xsy/Contest2/A/A.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+
+const int Max_n = 405;
+int n, m, s;
+int l[Max_n], r[Max_n];
+
+namespace Input {
+void main() {
+  n = read(), m = read(), s = read();
+  for (int i = 1; i <= n; i++) l[i] = read(), r[i] = read();
+}
+}  // namespace Input
+
+namespace Solve {
+void Solve2() {
+}
+void Solve3() {
+}
+void Solve4() {
+}
+void main() {
+}
+}  // namespace Solve
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("A.in", "r", stdin);
+  freopen("A.out", "w", stdout);
+#endif
+  Input::main();
+  Solve::main();
+}
