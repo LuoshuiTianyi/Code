843bc69d9e4318f58f067b0942ba4ab6a743e333
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-12-03 10:00:01 +0800

    changed

--- Luogu/P3803/3803.cpp
+++ Luogu/P3803/3803.cpp
@@ -38,11 +38,23 @@ int bit, len, rev[Max_n];
 void dft(cp *f, int t) {
   for (int i = 0; i < len; i++)
     if (rev[i] > i) swap(f[i], f[rev[i]]);
+  for (int l = 1; l < len; l <<= 1) {
+    cp Wn(cos(t * pi / (double)l), sin(t * pi / (double)l));
+    for (int i = 0; i < len; i += (l << 1)) {
+      cp Wnk(1, 0);
+      for (int k = i; k < i + l; k++) {
+        cp x = f[k], y = f[k + l] * Wnk;
+        f[k] = x + y, f[k + l] = x - y;
+        Wnk *= Wn;
+      }
+    }
+  }
 }
 void fft(cp *f, cp *g) {
   dft(f, 1), dft(g, 1);
   for (int i = 0; i < len; i++) f[i] *= g[i];
   dft(f, -1);
+  for (int i = 0; i < len; i++) f[i] /= (double)len;
 }
 void init(int n, int m) {
   bit = log2(n + m + 2) + 1;
@@ -56,6 +68,8 @@ namespace Solve {
 void main() {
   FFT::init(n, m);
   FFT::fft(f, g);
+  for (int i = 0; i <= n + m; i++)
+    printf("%d ", (int)(f[i].real() + 0.5));
 }
 }  // namespace Solve
 
