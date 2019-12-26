6746d7cfa65455eab04b8eb89ac646b8208b65cc
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-12-26 21:10:01 +0800

    changed

--- Luogu/P3690/3690.cpp
+++ Luogu/P3690/3690.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 using namespace std;
 #define LL long long
-#define go(x, i, v) for (int i = hd[x], v = to[i]; i; v = to[i = nx[i]])
 #define inline __inline__ __attribute__((always_inline))
 inline LL read() {
   LL x = 0, w = 1;
@@ -17,9 +16,53 @@ inline LL read() {
   }
   return x * w;
 }
+
+const int Max_n = 1e5 + 5;
+
+struct splay {
+  int fa, tag, v, sum, s[2];
+} k[Max_n];
+namespace LCT {
+#define ls(x) k[x].s[0]
+#define rs(x) k[x].s[1]
+bool Nrt(int x) {
+  return ls(k[x].fa) == x || rs(k[x].fa) == x;
+}
+void pushup(int x) {
+  k[x].sum = k[ls(x)].sum ^ k[rs(x)].sum ^ k[x].v;
+}
+void roll(int x) {
+  if (!x) return;
+  k[x].tag ^= 1, swap(ls(x), rs(x));
+}
+void pushdown(int x) {
+  if (k[x].tag) {
+    k[x].tag = 0;
+    roll(ls(x)), roll(rs(x));
+  }
+}
+void rotate(int x) {
+}
+}
+
+namespace Input {
+void main() {}
+}  // namespace Input
+
+namespace Init {
+void main() {}
+}  // namespace Init
+
+namespace Solve {
+void main() {}
+}  // namespace Solve
+
 int main() {
 #ifndef ONLINE_JUDGE
   freopen("3690.in", "r", stdin);
   freopen("3690.out", "w", stdout);
 #endif
+  Input::main();
+  Init::main();
+  Solve::main();
 }
