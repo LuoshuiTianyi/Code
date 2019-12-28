8859c878e11291821bfd99cf4b069cd38d05d6aa
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-12-28 10:20:01 +0800

    changed

--- Luogu/P3203/3203.cpp
+++ Luogu/P3203/3203.cpp
@@ -19,8 +19,40 @@ inline LL read() {
   return x * w;
 }
 
+const int Max_n = 2e5 + 5;
+int n, m;
+int a[Max_n];
+
+struct node {
+  int fa, siz, tag, s[2];
+} k[Max_n];
+namespace LCT {
+#define ls(x) k[x].s[0]
+#define rs(x) k[x].s[1]
+bool kd(int x) { return rs(k[x].fa) == x; }
+bool nrt(int x) { return kd(x) || ls(k[x].fa) == x; }
+void upd(int x) { k[x].siz = k[ls(x)].siz + k[rs(x)].siz + 1; }
+void roll(int x) {
+  if (!x) return;
+  swap(ls(x), rs(x)), k[x].tag ^= 1;
+}
+void pushdown(int x) {
+  if (k[x].tag) {
+    roll(ls(x)), roll(rs(x));
+    k[x].tag = 0;
+  }
+}
+void rotate(int x) {
+  int y = k[x].fa, z = k[y].fa, s1 = kd(x), s2 = k[x].s[!s1];
+  if (nrt(y)) k[z].s[kd(y)] = x;
+  k[x].s[!s1] = y;
+  if (s2) k[s2].fa = y;
+  k[x].fa = z, k[y].fa = x;
+}
+}  // namespace LCT
+
 namespace Input {
-void main() {}
+void main() { n = read(), m = read(); }
 }  // namespace Input
 
 namespace Init {
