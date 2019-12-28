e92cca02e27f2d0e502cb45712436e42f2301cb4
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2019-12-28 09:00:01 +0800

    changed

--- Luogu/P3690/3690.cpp
+++ Luogu/P3690/3690.cpp
@@ -33,18 +33,22 @@ void roll(int x) {
   if (!x) return;
   swap(ls(x), rs(x)), k[x].tag ^= 1; 
 }
-void pushdown(x) {
+void pushdown(int x) {
   if (k[x].tag) roll(ls(x)), roll(rs(x)), k[x].tag = 0;
 }
 void rotate(int x) {
   int y = k[x].fa, z = k[y].fa, s1 = kd(x), s2 = k[x].s[!s1];
-  if (nrt(y)) k[z].s[kd(y)] = x;
-  k[x].s[!s1] = y, k[y].s[s1] = s2;
-  if (s2) k[s2].fa = y;
-  k[x].fa = z, k[y].fa = x, upd(y);
+  
 }
 int stk[Max_n];
 void splay(int x) {
+  int top = 0, p = x;
+  for (; nrt(p); p = k[p].fa) stk[++top] = p;
+  stk[++top] = p;
+  while (top) pushdown(stk[top--]);
+  for (int fa = k[x].fa; nrt(x); rotate(x), fa = k[x].fa)
+    if (nrt(fa)) rotate(kd(fa) ^ kd(x) ? x : fa);
+  upd(x);
 }
 }  // namespace LCT
 
