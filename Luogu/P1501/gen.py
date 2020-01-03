b4f1ce5fa00eb40eaab4943c51e342bf89590b4e
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-01-03 20:20:01 +0800

    changed

--- Luogu/P1501/gen.py
+++ Luogu/P1501/gen.py
@@ -1,4 +1,13 @@
-from random import randint
+from random import randint, choice
+opt = ['+', '*', '/']
 n, Q = 3, 10
 print(n, Q)
-for i in range(n)
+for i in range(2, n + 1):
+    print(randint(1, i - 1), i)
+for i in range(Q):
+    op, u, v = choice(opt), randint(1, n), randint(1, n)
+    print(op, u, v, end = " ")
+    if op == '/':
+        print()
+    else:
+        print(randint(1, 3))
