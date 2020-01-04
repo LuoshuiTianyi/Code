8fd59eec4e310e82a84890982d0bbf487793f28c
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-01-04 20:10:02 +0800

    changed

---
+++ Codeforces/1284/A/A.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+
+const int Max_n = 25;
+int n, m, Q;
+int siz, p1[Max_n * Max_n], p2[Max_n * Max_n];
+int s1[Max_n][15], s2[Max_n][15];
+
+namespace Input {
+void main() {
+  n = read(), m = read();
+  for (int i = 1; i <= n; i++) scanf("%s", s1[i] + 1);
+  for (int i = 1; i <= m; i++) scanf("%s", s2[i] + 1);
+}
+}  // namespace Input
+
+namespace Init {
+void main() {
+  int pp = 1, qq = 1;
+  do {
+    pp = pp % n + 1;
+    qq = qq % m + 1;
+  } while (pp != 1 || qq != 1);
+}
+}  // namespace Init
+
+namespace Solve {
+void main() {}
+}  // namespace Solve
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("A.in", "r", stdin);
+  freopen("A.out", "w", stdout);
+#endif
+  Input::main();
+  Init::main();
+  Solve::main();
+}
---
+++ Codeforces/1284/B/B.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+
+namespace Input {
+void main() {}
+}  // namespace Input
+
+namespace Init {
+void main() {}
+}  // namespace Init
+
+namespace Solve {
+void main() {}
+}  // namespace Solve
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("B.in", "r", stdin);
+  freopen("B.out", "w", stdout);
+#endif
+  Input::main();
+  Init::main();
+  Solve::main();
+}
