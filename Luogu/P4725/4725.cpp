c155487af6ca3ddc3bbf3debc1daa054c064ef2a
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-01-11 20:30:02 +0800

    changed

--- Luogu/P4725/4725.cpp
+++ Luogu/P4725/4725.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <cstdio>
 #include <iostream>
 using namespace std;
@@ -19,8 +20,35 @@ inline LL read() {
   return x * w;
 }
 
-const int Max_n = 6e5 + 5;
+const int Max_n = 6e5 + 5, mod = 998244353, G = 3;
 int n;
+
+int bit, len, rev[Max_n];
+int ksm(int a, int b = mod - 2) {
+  int res = 1;
+  for (; b; b >>= 1, a = 1ll * a * a % mod)
+    if (b & 1) res = 1ll * res * a % mod;
+  return res;
+}
+struct poly {
+  int f[Max_n];
+  void dft(int t, int n) {
+    len = 1 << (bit = log2(n) + 1);
+    for (int i = 0; i < len; i++)
+      if (rev[i] = rev[i >> 1] >> 1 | ((i & 1) << (bit - 1)) > i)
+        swap(f[rev[i]], f[i]);
+    for (int l = 1; l < len; l <<= 1) {
+      int Wn = ksm(G, (mod - 1) / (l << 1));
+      for (int i = 0; i < len; i += l << 1) {
+        int Wnk = 1;
+        for (int k = i; k < i + l; k++, Wnk = 1ll * Wnk * Wn % mod) {
+          int x = f[k], y = 1ll * f[k + l] * Wnk % mod;
+          f[k] = (x + y) % mod, f[k + l] = (x - y + mod) % mod;
+        }
+      }
+    }
+  }
+};
 
 namespace Input {
 void main() {
