fa542ceb28e9894b7803c5d154e1d5a9f8e43457
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-01-11 09:10:01 +0800

    changed

--- Luogu/P4238/4238.cpp
+++ Luogu/P4238/4238.cpp
@@ -30,51 +30,15 @@ void main() {
 }  // namespace Input
 
 namespace Solve {
-int bit, len, F[Max_n], rev[Max_n];
-int ksm(int a, int b = mod - 2) {
-  int res = 1;
-  for (; b; b >>= 1, a = 1ll * a * a % mod)
-    if (b & 1) res = 1ll * res * a % mod;
-  return res;
-}
-void init(int n) {
-  len = 1 << (bit = log2(n * 3) + 1);
-  for (int i = 1; i < len; i++) rev[i] = rev[i >> 1] >> 1 | ((i & 1) << (bit - 1));
+int bit, len, rev[Max_n];
+void init(int deg) {
+  bit = log2(n * 3) + 1, len = 1 << bit;
 }
 void dft(int *f, int t) {
   for (int i = 0; i < len; i++)
-    if (rev[i] > i) swap(f[i], f[rev[i]]);
-  for (int l = 1; l < len; l <<= 1) {
-    int Wn = ksm(G, (mod - 1) / (l << 1));
-    if (t == -1) Wn = ksm(Wn);
-    for (int i = 0, Wnk = 1; i < len; i += l << 1, Wnk = 1)
-      for (int k = i; k < i + l; k++, Wnk = 1ll * Wnk * Wn % mod) {
-        int x = f[k], y = 1ll * f[k + l] * Wnk % mod;
-        f[k] = (x + y) % mod, f[k + l] = (x + mod - y) % mod;
-      }
-  }
-  if (t == -1) {
-    int Inv = ksm(len);
-    for (int i = 0; i < len; i++) f[i] = 1ll * f[i] * Inv % mod;
-  }
-}
-void solve(int deg, int *f, int *g) {
-  if (deg == 1) {
-    g[0] = ksm(f[0]);
-    return;
-  }
-  solve(deg + 1 >> 1, f, g), init(deg);
-  for (int i = 0; i < deg; i++) F[i] = f[i];
-  for (int i = deg; i < len; i++) F[i] = 0;
-  dft(F, 1), dft(g, 1);
-  for (int i = 0; i < len; i++)
-    g[i] = 1ll * (2ll - 1ll * F[i] * g[i] % mod + mod) * g[i] % mod;
-  dft(g, -1);
-  for (int i = deg; i < len; i++) g[i] = 0;
+    if (rev[i] > i) swap(f[rev[i]], f[i]);
 }
 void main() {
-  solve(n, a, ans);
-  for (int i = 0; i < n; i++) printf("%d ", ans[i]);
 }
 }  // namespace Solve
 
