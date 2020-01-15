fe6b3f96ac65431c5d660009c77247b669fac141
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-01-15 14:40:01 +0800

    changed

--- Luogu/P3317/3317.cpp
+++ Luogu/P3317/3317.cpp
@@ -22,23 +22,39 @@ inline LL read() {
 const int Max_n = 55;
 const double eps = 1e-8;
 int n;
-double b[Max_n][Max_n];
+double ans, b[Max_n][Max_n];
 
 namespace Input {
 void main() {
   n = read();
   for (int i = 1; i <= n; i++)
-    for (int j = 1; j <= n; j++)
-      scanf("%lf", &b[i][j]), b[i][j] = b[i][j] / (1.0 - b[i][j]);
+    for (int j = 1; j <= n; j++) {
+      scanf("%lf", &b[i][j]);
+      ans *= (1.0 - b[i][j]);
+      if (b[i][j] > 1 - eps) b[i][j] = 1 - eps;
+      b[i][j] = -(b[i][j] / (1.0 - b[i][j]));
+      if (i == j) b[i][j] += 1.0;
+    }
 }
 }  // namespace Input
 
-namespace Init {
-void main() {}
-}  // namespace Init
-
 namespace Solve {
-void main() {}
+double Guess(int n) {
+  double ans = 1;
+  for (int i = 1; i <= n; i++) {
+    int mx = i;
+    for (int j = i + 1; j <= n; j++)
+      if (b[j][i] > b[mx][i]) mx = j;
+    if (mx != i) swap(b[i], b[mx]), ans = -ans;
+    if (b[i][i] < eps || b[i][i] > 1 - eps) return 0;
+    for (int j = i + 1; j <= n; j++) {
+      double d = b[j][i] / b[i][i];
+    }
+  }
+}
+void main() {
+  ans *= Guess(n - 1);
+}
 }  // namespace Solve
 
 int main() {
@@ -47,6 +63,5 @@ int main() {
   freopen("3317.out", "w", stdout);
 #endif
   Input::main();
-  Init::main();
   Solve::main();
 }
