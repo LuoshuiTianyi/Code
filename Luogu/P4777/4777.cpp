af4bf3d6f2c83d58da3eb9a006e43a17b014da25
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-05-10 19:50:01 +0800

    changed

--- Luogu/P4777/4777.cpp
+++ Luogu/P4777/4777.cpp
@@ -1,12 +1,42 @@
 #include <cstdio>
 #include <iostream>
 using namespace std;
-int n;
-long long a[100001], b[100001], gcd;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+
+namespace Input {
+void main() {}
+}  // namespace Input
+
+namespace Init {
+void main() {}
+}  // namespace Init
+
+namespace Solve {
+void main() {}
+}  // namespace Solve
+
 int main() {
+#ifndef ONLINE_JUDGE
   freopen("4777.in", "r", stdin);
   freopen("4777.out", "w", stdout);
-  cin >> n;
-  for (int i = 1; i <= n; i++)
-    scanf("%d%d", &a[i], &b[i]);
+#endif
+  Input::main();
+  Init::main();
+  Solve::main();
 }
