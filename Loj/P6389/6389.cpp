585e76e90eebe4a1c4d6dbe8f6d3f6d23206ff87
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-05-14 16:20:01 +0800

    changed

---
+++ Loj/P6389/6389.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+
+const int Max_n = 3e4 + 5;
+
+namespace Input {
+void main() {}
+}  // namespace Input
+
+namespace Init {
+void main() {}
+}  // namespace Init
+
+namespace Solve {
+void main() {}
+}  // namespace Solve
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("6389.in", "r", stdin);
+  freopen("6389.out", "w", stdout);
+#endif
+  Input::main();
+  Init::main();
+  Solve::main();
+}
