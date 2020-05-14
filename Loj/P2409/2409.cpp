9e40138388cde1c127a85cd878473e56270132d2
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-05-14 20:10:02 +0800

    changed

---
+++ Loj/P2409/2409.cpp
@@ -0,0 +1,69 @@
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+
+const int Max_n = 8e5 + 5, mod = 998244353;
+int T;
+int n, a[Max_n];
+vector<int> f[;
+
+namespace Input {
+void main() {
+  n = read();
+  for (int i = 1; i <= n; i++) a[i] = read();
+}
+}  // namespace Input
+
+namespace Poly {
+int len, bit, rev[Max_n];
+int ksm(int a, int b = mod - 2) {
+  int res = 1;
+  for (; b; b >>= 1, a = (LL)a * a % mod)
+    if (b & 1) res = (LL)res * a % mod;
+  return res;
+}
+void init(int n) {
+  len = 1 << (bit = log2(n) + 1);
+  for (int i = 0; i < len; i++)
+    rev[i] = rev[i >> 1] >> 1 | ((i & 1) << bit - 1);
+}
+void dft(int *f, bool t) {
+  for (int i = 0; i < len; i++)
+    if (rev[i] > i) swap(f[i], rev[i]);
+  for (int i = 
+}
+}  // namespace Poly
+
+namespace Solve {
+void main() {}
+}  // namespace Solve
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("2409.in", "r", stdin);
+  freopen("2409.out", "w", stdout);
+#endif
+  T = read();
+  while (T--) {
+    Input::main();
+    Solve::main();
+  }
+}
