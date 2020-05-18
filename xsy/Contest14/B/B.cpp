df3c7d8f49ea65945c52e7e051470aa4e646b28e
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-05-18 14:00:02 +0800

    changed

---
+++ xsy/Contest14/B/B.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+#define inline __inline__ __attribute__((always_inline))
+inline LL read() {
+  LL x = 0, w = 1;
+  char ch = getchar();
+  while (!isdigit(ch)) {
+    if (ch == '-') w = -1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) {
+    x = (x << 3) + (x << 1) + ch - '0';
+    ch = getchar();
+  }
+  return x * w;
+}
+
+const int Max_n = 1e6 + 5;
+int n, m, L;
+int w[Max_n];
+struct graph {
+  int hd[Max_n];
+  int cntr, nx[Max_n << 1], to[Max_n << 1];
+  void addr(int u, int v) {
+    cntr++;
+    nx[cntr] = hd[u], to[cntr] = v;
+    hd[u] = cntr;
+  }
+} G;
+
+namespace Input {
+void main() {
+  n = read(), m = read(), L = read();
+  for (int i = 2; i <= n; i++) G.addr(read(), i);
+  for (int i = 1; i <= n; i++) w[i] = read();
+}
+}  // namespace Input
+
+namespace Init {
+void main() {}
+}  // namespace Init
+
+namespace Solve {
+void main() {}
+}  // namespace Solve
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("B.in", "r", stdin);
+  freopen("B.out", "w", stdout);
+#endif
+  Input::main();
+  Init::main();
+  Solve::main();
+}
