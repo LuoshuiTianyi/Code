0d9b7bd6a78cf21330bc3d5447ab3679a4920eec
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-05-19 14:20:01 +0800

    changed

--- xsy/Contest6/A/A.cpp
+++ xsy/Contest6/A/A.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <iostream>
+#include <vector>
 using namespace std;
 #define LL long long
 #define go(G, x, i, v) \
@@ -20,19 +21,56 @@ void read(T &x) {
   if (fl) x = -x;
 }
 
-const int Max_n = 1e6 + 5;
-
+const int Max_n = 2e6 + 5, V = 1e5;
+int n, m, ans;
+struct edge {
+  int u, v, w;
+} e[Max_n];
+vector<int> M[(V << 2) + 10];
 
 namespace Input {
-void main() { fread(s, 1, 1 << 25, stdin); }
+void main() {
+  fread(s, 1, 1 << 25, stdin); 
+  read(n), read(m), ans = n + 1;
+  for (int i = 1; i <= m; i++)
+    read(e[i].u), read(e[i].v), read(e[i].w);
+}
 }  // namespace Input
 
 namespace Init {
-void main() {}
+int L, R, x;
+#define ls (o << 1)
+#define rs (o << 1 | 1)
+#define mid (l + r >> 1)
+void add(int o, int l, int r) {
+  if (R < L) return;
+  if (l >= L && r <= R) {
+    M[o].push_back(x);
+    return;
+  }
+  if (mid >= L) add(ls, l, mid);
+  if (mid < R) add(rs, mid + 1, r);
+}
+void main() {
+  for (int i = 1; i <= m; i++) {
+    x = i;
+    L = 1, R = e[i].w - 1, add(1, 1, V);
+    L = e[i].w + 1, R = V, add(1, 1, V);
+  }
+}
 }  // namespace Init
 
 namespace Solve {
-void main() {}
+int fa[Max_n];
+int top, stk[Max_n];
+void dfs(int x, int l, int r) {
+  if (l == r) {
+  }
+}
+void main() {
+  for (int i = 1; i <= n; i++) 
+  dfs(1, 1, V);
+}
 }  // namespace Solve
 
 int main() {
