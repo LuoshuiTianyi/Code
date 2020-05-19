360cd659cba91459a6f4934d8539bc9c02fb471e
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-05-19 19:00:01 +0800

    changed

---
+++ xsy/Contest8/B/B.cpp
@@ -0,0 +1,50 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+
+char s[1 << 25], *S = s;
+#define getchar() (*S++)
+template <typename T>
+void read(T &x) {
+  char ch = getchar();
+  int fl = 0;
+  x = 0;
+  while (!isdigit(ch)) {
+    if (ch == '-') fl = 1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) x = (x << 1) + (x << 3) + ch - '0', ch = getchar();
+  if (fl) x = -x;
+}
+
+const int Max_n =  3e5 + 5;
+int n, m;
+LL Ans[Max_n];
+struct que {
+  int id, l, r;
+} q[Max_n];
+
+namespace Input {
+void main() { fread(s, 1, 1 << 25, stdin); }
+}  // namespace Input
+
+namespace Init {
+void main() {}
+}  // namespace Init
+
+namespace Solve {
+void main() {}
+}  // namespace Solve
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("B.in", "r", stdin);
+  freopen("B.out", "w", stdout);
+#endif
+  Input::main();
+  Init::main();
+  Solve::main();
+}
