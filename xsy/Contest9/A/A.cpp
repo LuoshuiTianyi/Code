379fb236681b1de8b8c284577f500864436c09a8
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-05-20 21:00:02 +0800

    changed

--- xsy/Contest9/A/A.cpp
+++ xsy/Contest9/A/A.cpp
@@ -30,24 +30,44 @@ void rstr(char *s) {
   }
 }
 
-const int Max_n = 1e5 + 5;
+const int Max_n = 2e5 + 5;
 int n, Q;
 int a[Max_n], l[Max_n], r[Max_n];
+bool vis[Max_n];
 LL ans[Max_n];
-priority_queue<pair<int, int> > q;
+priority_queue<pair<int, LL> > q;
 
 namespace Input {
 void main() { 
   fread(read_str, 1, 1 << 25, stdin); 
-  read(n), read(Q);
-  for (int i = 1; i <= n; i++) a[i] = read(), l[i] = i - 1, r[i] = i + 1, q.push(make_pair(a[i], i));
+  read(n);
+  for (int i = 1; i <= n; i++) read(a[i]), l[i] = i - 1, r[i] = i + 1, q.push(make_pair(a[i], i));
   r[n] = 0;
 }
 }  // namespace Input
 
 namespace Solve {
 void main() {
+  int x;
   for (int i = 1; i <= (n + 1 >> 1); i++) {
+    ans[i] = ans[i - 1];
+    for (x = q.top().second; vis[x]; q.pop(), x = q.top().second);
+    q.pop(), ans[i] += a[x];
+    a[x] = a[l[x]] + a[r[x]] - a[x], vis[l[x]] = vis[r[x]] = 1;
+    if (l[x] && r[x]) {
+      q.push(make_pair(a[x], x));
+      l[x] = l[l[x]], r[x] = r[r[x]];
+      r[l[x]] = x, l[r[x]] = x;
+    } else if (l[x]) {
+      r[l[l[x]]] = 0;
+    } else if (r[x]) {
+      l[r[r[x]]] = 0;
+    }
+  }
+  read(Q);
+  while (Q--) {
+    read(x);
+    printf("%lld\n", ans[x]);
   }
 }
 }  // namespace Solve
