80da6b2b2e04a5a8a7e71154e180393bb5fc566c
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-05-24 19:50:01 +0800

    changed

--- xsy/Contest5/A/A.cpp
+++ xsy/Contest5/A/A.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cstdio>
 #include <iostream>
+#include <vector>
 using namespace std;
 #define LL long long
 #define go(G, x, i, v) \
@@ -29,10 +30,19 @@ void rstr(char *s) {
   }
 }
 
-const int Max_n = 100, mod = 1e9 + 7;
+const int Max_n = 100, Max_s = 1 << 15 | 1, mod = 1e9 + 7;
 int n, m;
 int l[Max_n], r[Max_n], l2[Max_n], r2[Max_n];
-int cnt, all[Max_n],fa[Max_n];
+int cnt, ct, all[Max_n], fa[Max_n], id[Max_n];
+int val[Max_n], g[Max_s], f[10][Max_s];
+vector Con[Max_n];
+
+int ksm(int a, int b = mod - 2) {
+  int res = 1;
+  for (; b; b >>= 1, a = (LL)a * a % mod)
+    if (b & 1) res = (LL)res * a % mod;
+  return res;
+}
 
 namespace Input {
 void main() { 
@@ -44,12 +54,20 @@ void main() {
 }  // namespace Input
 
 namespace Init {
+int find(int x) { return x == fa[x] ? x : fa[x] = find(fa[x]); }
 void main() {
   sort(all + 1, all + cnt + 1);
   cnt = unique(all + 1, all + cnt + 1) - all - 1;
+  for (int i = 1; i <= cnt; i++) fa[i] = i;
   for (int i = 1; i <= m; i++) {
     l2[i] = lower_bound(all + 1, all + cnt + 1, l[i]) - all;
     r2[i] = lower_bound(all + 1, all + cnt + 1, r[i]) - all;
+    fa[find(r2[i])] = find(l2[i]);
+  }
+  for (int i = 1; i <= cnt; i++) ksm(10, all[i] - all[i - 1]);
+  for (int i = 1; i <= cnt; i++) {
+    if (find(i) == i) id[i] = ++ct;
+    Con[id[find(i)]].push_back(i);
   }
 }
 }  // namespace Init
