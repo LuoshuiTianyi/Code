f558e998a4cce3911261eb6478eae3c4eece162b
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-05-26 21:10:02 +0800

    changed

--- xsy/Contest9/B/B.cpp
+++ xsy/Contest9/B/B.cpp
@@ -31,30 +31,59 @@ void rstr(char *s) {
 
 const int Max_n = 5e5 + 5;
 int n, Q, Ans[Max_n];
-int s[Max_n], l[Max_n], r[Max_n];
-int stk[Max_n];
+int l[Max_n], r[Max_n];
+int top, stk[Max_n];
 char S[Max_n];
 vector<int> M[Max_n];
 
 namespace Input {
-void main() { 
-  fread(read_str, 1, 1 << 25, stdin); 
+void main() {
+  fread(read_str, 1, 1 << 25, stdin);
   n = read(), rstr(S + 1);
   Q = read();
-  for (int i = 1; i <= n; i++) {
+  for (int i = 1; i <= Q; i++) {
+    l[i] = read(), M[r[i] = read()].push_back(i);
   }
 }
 }  // namespace Input
 
-namespace {
+namespace SegTree {
+int Min[Max_n << 1], tag[Max_n << 1];
+#define ls (o << 1)
+#define rs (o << 1 | 1)
+#define mid (l + r >> 1)
+void pushdown(int x) { Min[ls] += tag[x], Min[rs] += tag[x], tag[x] = 0; }
+void pushup(int x) { Min[x] = min(Min[ls], Min[rs]); }
+void add(int o, int l, int r, int L, int R, int x) {
+  if (l >= L && r <= R) {
+    Min[o] += x, tag[o] += x;
+    return;
+  }
+  pushdown(x);
+  if (mid >= L) add(ls, l, mid, L, R, x);
+  if (mid < R) add(rs, mid + 1, r, L, R, x);
+  pushup(x);
 }
-
-namespace Init {
-void main() {}
-}  // namespace Init
+int query(int o, int l, int r, int L, int R) {
+  int ans = 1e9;
+  if (l >= L && r <= R) return Min[o];
+  pushdown(x);
+  if (mid >= L) ans = min(ans, add(ls, l, mid, L, R));
+  if (mid < R) ans = min(ans, add(rs, mid + 1, r, L, R));
+  pushup(x);
+  return ans;
+}
+}  // namespace SegTree
+using namespace SegTree;
 
 namespace Solve {
-void main() {}
+void main() {
+  for (int i = 1; i <= n; i++) {
+    if (S[i] == 'C') {
+      if (top) add(
+    }
+  }
+}
 }  // namespace Solve
 
 int main() {
@@ -63,6 +92,5 @@ int main() {
   freopen("B.out", "w", stdout);
 #endif
   Input::main();
-  Init::main();
   Solve::main();
 }
