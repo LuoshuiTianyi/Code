b3404126fbced220cb7d1ab4b9185ac345226b40
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-06-02 15:20:01 +0800

    changed

--- Codeforces/102439/A/A.cpp
+++ Codeforces/102439/A/A.cpp
@@ -34,6 +34,7 @@ void rstr(char *s) {
 const int Max_n = 1e5 + 5;
 const LD eps = 1e-8;
 int n, id[Max_n];
+int top, stk[Max_n];
 LD a, b;
 struct light {
   LD x, v;
@@ -49,36 +50,40 @@ void main() {
 }  // namespace Input
 
 bool pd(LD v0, LD v, LD d) {
-  return sqrt(v0 * v0 - 2.0 * b * d);
+  return (v0 * v0 - 2.0 * b * d) <= v * v;
 }
-
 namespace Init {
 bool cmp(light a, light b) { return a.x < b.x; }
-int top, stk[Max_n];
 void main() {
   for (int i = 1; i <= n; i++) {
-    while (top && pd(k[stk[top]].v, k[i].v, k[i].x - k[stk[top]].x)) top--;
+    while (top && !pd(k[stk[top]].v, k[i].v, k[i].x - k[stk[top]].x)) top--;
     stk[++top] = i;
   }
 }
 }  // namespace Init
 
 namespace Solve {
-LD v[Max_n];
+LD v[Max_n], Ans[Max_n];
 bool check(LD t, LD v0, LD lim, LD d) {
-  
+  return pd(v0 + a * t, lim, d - v0 * t - 0.5 * a * t * t);
 }
 void main() {
   for (int i = 0; i < top; i++) {
     light x = k[stk[i]], y = k[stk[i + 1]];
     LD v0 = v[i], d = y.x - x.x, v1 = y.v;
-    LD l = 0, r = 1e4, ans;
+    LD l = 0, r = 1e4, ans = 0;
     while (r - l > eps) {
       LD mid = (l + r) / 2;
       if (check(mid, v0, v1, d)) l = ans = mid;
       else r = mid;
     }
+    Ans[i + 1] = Ans[i] + ans;
+    //printf("%.7Lf\n", ans);
+    d -= v0 * ans - 0.5 * a * ans * ans;
+    v[i + 1] = sqrt(v0 * v0 - 2.0 * b * d);
+    Ans[i + 1] += (v0 + ans * a - v[i + 1]) / b;
   }
+  printf("%.7Lf", Ans[top]);
 }
 }  // namespace Solve
 
