f949334aa452f9dcf063bde4b5474b0f14df7381
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-06-02 14:50:01 +0800

    changed

---
+++ Codeforces/102439/A/A.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define Ld long double
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+
+char read_str[1 << 25], *CH = read_str;
+#define getchar() (*CH++)
+LL read() {
+  char ch = getchar();
+  int fl = 0;
+  LL x = 0;
+  while (!isdigit(ch)) {
+    if (ch == '-') fl = 1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) x = (x << 1) + (x << 3) + ch - '0', ch = getchar();
+  return fl ? -x : x;
+}
+void rstr(char *s) {
+  char *x = s;
+  char ch = getchar();
+  while (ch == ' ' || ch == '\n' || ch == '\r') ch = getchar();
+  while (ch != ' ' && ch != '\n' && ch != '\r') {
+    *x++ = ch;
+    ch = getchar();
+  }
+}
+
+const int Max_n = 1e5 + 5;
+int n;
+double a, b;
+double x[Max_n], v[Max_n];
+
+namespace Input {
+void main() { 
+  fread(read_str, 1, 1 << 25, stdin); 
+  n = read(), a = read(), b = read();
+}
+}  // namespace Input
+
+namespace Init {
+void main() {}
+}  // namespace Init
+
+namespace Solve {
+void main() {}
+}  // namespace Solve
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("A.in", "r", stdin);
+  freopen("A.out", "w", stdout);
+#endif
+  Input::main();
+  Init::main();
+  Solve::main();
+}
