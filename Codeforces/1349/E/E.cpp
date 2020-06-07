0c793a46eb6c7d84c9e784e0bb555a1f167f58cd
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-06-07 21:20:01 +0800

    changed

---
+++ Codeforces/1349/E/E.cpp
@@ -0,0 +1,60 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+#define LL long long
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+
+char read_str[1 << 25], *CH = read_str;
+#define getchar() (*CH++)
+LL read() {
+  char ch = getchar();
+  int fl = 0;
+  LL x = 0;
+  while (!isdigit(ch)) {
+    if (ch == '-') fl = 1;
+    ch = getchar();
+  }
+  while (isdigit(ch)) x = (x << 1) + (x << 3) + ch - '0', ch = getchar();
+  return fl ? -x : x;
+}
+void rstr(char *s) {
+  char *x = s;
+  char ch = getchar();
+  while (ch == ' ' || ch == '\n' || ch == '\r') ch = getchar();
+  while (ch != ' ' && ch != '\n' && ch != '\r') {
+    *x++ = ch;
+    ch = getchar();
+  }
+}
+
+const int Max_n = 2e5 + 5;
+int n, nu;
+LL Max, a[Max_n];
+
+namespace Input {
+void main() { 
+  fread(read_str, 1, 1 << 25, stdin); 
+  n = read();
+  for (int i = 1; i <= n; i++) {
+    a[i] = read();
+    if (a[i] > Max) Max = a[i], nu = 0;
+    nu += a[i] == Max;
+  }
+}
+}  // namespace Input
+
+namespace Solve {
+void main() {
+  
+}
+}  // namespace Solve
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("E.in", "r", stdin);
+  freopen("E.out", "w", stdout);
+#endif
+  Input::main();
+  Solve::main();
+}
