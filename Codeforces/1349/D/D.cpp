5b6ea2820c1b54881c588b8a4590c9001408f496
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-06-09 10:50:01 +0800

    changed

--- Codeforces/1349/D/D.cpp
+++ Codeforces/1349/D/D.cpp
@@ -28,16 +28,36 @@ void rstr(char *s) {
   }
 }
 
+const int Max_n = 3e5 + 5, mod = 998244353;
+int n, m, a[Max_n];
+int f[Max_n];
+
+int ksm(int a, int b = mod - 2) {
+  int res = 1;
+  for (; b; b >>= 1, a = (LL)a * a % mod)
+    if (b & 1) res = (LL)res * a % mod;
+  return res;
+}
+
 namespace Input {
-void main() { fread(read_str, 1, 1 << 25, stdin); }
+void main() {
+  fread(read_str, 1, 1 << 25, stdin); 
+  n = read();
+  for (int i = 1; i <= n; i++) m += a[i] = read();
+}
 }  // namespace Input
 
-namespace Init {
-void main() {}
-}  // namespace Init
-
 namespace Solve {
-void main() {}
+void main() {
+  f[0] = n - 1;
+  int t = (LL)m * (n - 1) % mod;
+  for (int i = 1; i < m; i++)
+    f[i] = ((LL)i * (n - 1) % mod * f[i - 1] % mod + t) * ksm(m + i) % mod;
+  for (int i = m - 1; ~i; i--) (f[i] += f[i + 1]) %= mod;
+  int ans = 0;
+  for (int i = 1; i <= n; i++) (ans += f[a[i]]) %= mod;
+  ans += mod - (LL)(n - 1) * 
+}
 }  // namespace Solve
 
 int main() {
@@ -46,6 +66,5 @@ int main() {
   freopen("D.out", "w", stdout);
 #endif
   Input::main();
-  Init::main();
   Solve::main();
 }
