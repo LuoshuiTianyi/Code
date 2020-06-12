6e0a055933c9374528ac9e7868c7ab7f6a0ba3c3
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-06-12 21:50:01 +0800

    changed

--- Luogu/P4238/4238.cpp
+++ Luogu/P4238/4238.cpp
@@ -1,100 +1,108 @@
 #include <cmath>
 #include <cstdio>
-#include <cstring>
 #include <iostream>
 using namespace std;
 #define LL long long
-#define inline __inline__ __attribute__((always_inline))
-inline LL read() {
-  LL x = 0, w = 1;
+#define Mod(x) (x >= mod ? x -= mod : 0)
+#define go(G, x, i, v) \
+  for (int i = G.hd[x], v = G.to[i]; i; v = G.to[i = G.nx[i]])
+
+char read_str[1 << 25], *CH = read_str;
+#define getchar() (*CH++)
+LL read() {
   char ch = getchar();
+  int fl = 0;
+  LL x = 0;
   while (!isdigit(ch)) {
-    if (ch == '-') w = -1;
+    if (ch == '-') fl = 1;
     ch = getchar();
   }
-  while (isdigit(ch)) {
-    x = (x << 3) + (x << 1) + ch - '0';
+  while (isdigit(ch)) x = (x << 1) + (x << 3) + ch - '0', ch = getchar();
+  return fl ? -x : x;
+}
+void rstr(char *s) {
+  char *x = s;
+  char ch = getchar();
+  while (ch == ' ' || ch == '\n' || ch == '\r') ch = getchar();
+  while (ch != ' ' && ch != '\n' && ch != '\r') {
+    *x++ = ch;
     ch = getchar();
   }
-  return x * w;
 }
 
-const int Max_n = 6e5 + 5, mod = 998244353, G = 3;
-int n;
+const int Max_n = 6e5 + 5, Len = 1 << 20;
+const unsigned long long mod = 998244353;
+int n, a[Max_n], ans[Max_n];
+int gp[Len | 1];
+
+namespace Input {
+void main() { 
+  fread(read_str, 1, 1 << 25, stdin); 
+  n = read();
+  for (int i = 0; i < n; i++) a[i] = read();
+}
+}  // namespace Input
+
 int ksm(int a, int b = mod - 2) {
   int res = 1;
-  for (; b; b >>= 1, a = 1ll * a * a % mod)
-    if (b & 1) res = 1ll * res * a % mod;
+  for (; b; b >>= 1, a = (LL)a * a % mod)
+    if (b & 1) res = (LL)res * a % mod;
   return res;
 }
+namespace Init {
+void main() {
+  gp[0] = 1;
+  int g = ksm(3, (mod - 1) / Len);
+  for (int i = 1; i <= Len; i++) gp[i] = (LL)gp[i - 1] * g % mod;
+}
+}  // namespace Init
 
 namespace Poly {
-int len, bit, rev[Max_n];
+int bit, len, rev[Max_n];
 void init(int n) {
   len = 1 << (bit = log2(n) + 1);
-  for (int i = 0; i < len; i++)
-    rev[i] = rev[i >> 1] >> 1 | ((i & 1) << (bit - 1));
+  for (int i = 0; i < len; i++) rev[i] = rev[i >> 1] >> 1 | ((i & 1) << bit - 1);
 }
-struct poly {
-  int f[Max_n];
-  int& operator[](int x) { return f[x]; }
-  void init() {
-    memset(f, 0, sizeof f);
-  }
-  void dft(int t) {
-    for (int i = 0; i < len; i++)
-      if (rev[i] > i) swap(f[rev[i]], f[i]);
-    for (int l = 1; l < len; l <<= 1) {
-      int Wn = ksm(3, (mod - 1) / (l << 1));
-      if (t) Wn = ksm(Wn);
-      for (int i = 0; i < len; i += l << 1) {
-        int Wnk = 1;
-        for (int k = i; k < i + l; k++, Wnk = 1ll * Wnk * Wn % mod) {
-          int x = f[k], y = 1ll * f[k + l] * Wnk % mod;
-          f[k] = (x + y) % mod, f[k + l] = (x - y + mod) % mod;
-        }
+void dft(int *f, bool t) {
+  for (int i = 0; i < len; i++)
+    if (rev[i] > i) swap(f[i], f[rev[i]]);
+  for (int l = 1; l < len; l <<= 1) {
+    int Wn = ksm(3, (mod - 1) / (l << 1));
+    if (t) Wn = ksm(Wn);
+    for (int i = 0; i < len; i += l << 1) {
+      int Wnk = 1;
+      for (int k = i; k < i + l; k++, Wnk = (LL)Wnk * Wn % mod) {
+        int x = f[k];
+        int y = (LL)f[k + l] * Wnk % mod;
+        f[k] = x + y, Mod(f[k]);
+        f[k + l] = x - y + mod, Mod(f[k + l]);
       }
     }
-    if (t)
-      for (int i = 0, Inv = ksm(len); i < len; i++)
-        f[i] = 1ll * f[i] * Inv % mod;
   }
-};
-void Mul(poly &f, poly &g, int N) {
-  init(N);
-  f.dft(0), g.dft(0);
-  for (int i = 0; i < len; i++) f[i] = (LL)f[i] * g[i] % mod;
-  f.dft(1), g.dft(1);
+  if (t)
+    for (int i = 0, Inv = ksm(len); i < len; i++) 
+      f[i] = 1ll * f[i] * Inv % mod;
 }
-void Inv(poly &f, poly &g, int N) {
-  static poly F;
-  g.init(), g[0] = ksm(f[0]);
+void Inv(int *f, int *inv, int N) {
+  static int F[Max_n];
+  inv[0] = ksm(f[0]), inv[1] = 0;
   for (int deg = 2; deg < (N << 1); deg <<= 1) {
     init(deg * 3);
     for (int i = 0; i < deg; i++) F[i] = f[i];
-    for (int i = deg; i < len; i++) F[i] = 0;
-    F.dft(0), g.dft(0);
+    for (int i = deg; i < len; i++) F[i] = inv[i] = 0;
+    dft(F, 0), dft(inv, 0);
     for (int i = 0; i < len; i++) 
-      g[i] = (2ll * g[i] % mod + mod - (LL)g[i] * g[i] % mod * F[i] % mod) % mod;
-    g.dft(-1);
-    for (int i = deg; i < len; i++) g[i] = 0;
+      inv[i] = (LL)inv[i] * (mod + 2 - (LL)F[i] * inv[i] % mod) % mod;
+    dft(inv, 1);
+    for (int i = deg; i < len; i++) inv[i] = 0;
   }
 }
 }  // namespace Poly
 using namespace Poly;
 
-poly f, ans;
-
-namespace Input {
-void main() { 
-  n = read(); 
-  for (int i = 0; i < n; i++) f[i] = read();
-}
-}  // namespace Input
-
 namespace Solve {
 void main() {
-  Inv(f, ans, n);
+  Inv(a, ans, n);
   for (int i = 0; i < n; i++) printf("%d ", ans[i]);
 }
 }  // namespace Solve
@@ -105,5 +113,6 @@ int main() {
   freopen("4238.out", "w", stdout);
 #endif
   Input::main();
+  Init::main();
   Solve::main();
 }
