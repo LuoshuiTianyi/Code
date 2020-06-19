705815d1ee0320528274ff48659b55401d3d772f
LuoshuiTianyi <CSYcaosiyu@gmail.com>
2020-06-19 09:50:01 +0800

    changed

--- Luogu/P3690/3690.cpp
+++ Luogu/P3690/3690.cpp
@@ -29,24 +29,16 @@ void rstr(char *s) {
 }
 
 const int Max_n = 1e5 + 5;
-int n, m;
-int a[Max_n];
-
-namespace Input {
-void main() { 
-  fread(read_str, 1, 1 << 25, stdin); 
-  n = read(), m = read();
-}
-}  // namespace Input
 
 namespace LCT {
 #define ls(x) k[x].s[0]
 #define rs(x) k[x].s[1]
+#define fa(x) k[x].fa
 struct node {
-  int fa, val, rev_tag, xor_sum, s[2];
+  int fa, s[2], rev_tag, xor_sum, val;
 } k[Max_n];
-bool kd(int x) { return rs(k[x].fa) == x; }
-bool nrt(int x) { return ls(k[x].fa) == x || kd(x); }
+bool kd(int x) { return rs(fa(x)) == x; }
+bool nrt(int x) { return kd(x) || ls(fa(x)) == x; }
 void upd(int x) {
   k[x].xor_sum = k[x].val ^ k[ls(x)].xor_sum ^ k[rs(x)].xor_sum;
 }
@@ -54,78 +46,21 @@ void roll(int x) {
   if (!x) return;
   swap(ls(x), rs(x)), k[x].rev_tag ^= 1;
 }
-void pushdown(int x) {
+void pushdown (int x) {
   if (k[x].rev_tag) roll(ls(x)), roll(rs(x)), k[x].rev_tag = 0;
 }
-void rotate(int x) {
-  int y = k[x].fa, z = k[y].fa, s1 = kd(x), s2 = k[x].s[!s1];
-  if (nrt(y)) k[z].s[kd(y)] = x;
-  k[x].s[!s1] = y, k[y].s[s1] = s2;
-  if (s2) k[s2].fa = y;
-  k[y].fa = x, k[x].fa = z;
-  upd(y);
 }
-void Push(int x) {
-  if (nrt(x)) Push(k[x].fa);
-  pushdown(x);
-}
-void splay(int x) {
-  Push(x);
-  for (int fa = k[x].fa; nrt(x); rotate(x), fa = k[x].fa)
-    if (nrt(fa)) rotate(kd(fa) ^ kd(x) ? x : fa);
-  upd(x);
-}
-void access(int x) {
-  for (int y = 0; x; x = k[y = x].fa) splay(x), rs(x) = y;
-}
-void makert(int x) { access(x), splay(x), roll(x); }
-int findrt(int x) {
-  access(x), splay(x);
-  while (ls(x)) x = ls(x);
-  splay(x);
-  return x;
-}
-void link(int u, int v) {
-  makert(u);
-  if (findrt(v) != u) k[u].fa = v;
-}
-void cut(int u, int v) {
-  makert(u);
-  if (findrt(v) == u && k[v].fa == u && !ls(v)) rs(u) = k[v].fa = 0;
-}
-int qry(int u, int v) {
-  makert(u), access(v), splay(v);
-  return k[v].xor_sum;
-}
-}  // namespace LCT
-using namespace LCT;
+
+namespace Input {
+void main() { fread(read_str, 1, 1 << 25, stdin); }
+}  // namespace Input
 
 namespace Init {
-void main() {
-  for (int i = 1; i <= n; i++) k[i].val = read();
-}
+void main() {}
 }  // namespace Init
 
 namespace Solve {
-void main() {
-  while (m--) {
-    int op = read(), u = read(), v = read();
-    switch(op) {
-    case 0:
-      printf("%d\n", qry(u, v));
-      break;
-    case 1:
-      link(u, v);
-      break;
-    case 2:
-      cut(u, v);
-      break;
-    case 3:
-      splay(u), k[u].val = v;
-      break;
-    }
-  }
-}
+void main() {}
 }  // namespace Solve
 
 int main() {
